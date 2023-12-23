5097d8e432ab5929cefa3c152551a2f198b3669e
石黒みなみ <ishigurominami142@gmail.com>
2023-12-23 13:16:31 +0900

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+//1. 変数にtrueを代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。
+
+var boolean = true;
+if (boolean) {
+console.log(boolean);
+};
+
+//2. 変数に100を代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。
+
+var num = 100;
+if (num === 100) {
+  console.log(num);
+}
+
+//3. 変数に"JavaScript"を代入し、ifの条件がtrueの時にconsole.log();で変数の中身を表示させる処理を作ってください。
+
+var text = "JavaScript"
+if (text == "JavaScript") {
+console.log(text);
+}
